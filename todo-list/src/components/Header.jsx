57f53a77763e91d636b1c29380058e88e8802f51
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-08-30 11:46:58 -0300

    Header finalizado

--- todo-list/src/components/Header.jsx
+++ todo-list/src/components/Header.jsx
@@ -6,7 +6,8 @@ import logo from '../assets/logo.svg';
 export function Header() {
     return(
         <header className={styles.header}>
-            <strong><img src={logo} alt="Logo do Projeto"></img>Todo</strong>
+            <img src={logo} alt="Logo do Projeto"></img>
+            <strong className={styles.to}>to</strong><strong className={styles.do}>do</strong>
         </header>
     );
 }
